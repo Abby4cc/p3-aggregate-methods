537951a2099f164eb5132785d937f91e91c3fee3
Abigail Chelanagt <abigailchelah@gmail.com>
2025-05-23 22:14:38 +0300

    Add my files

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
@@ -15,9 +16,24 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        """Return the number of courses the student is enrolled in."""
+        return len(self._enrollments)
+
+    def aggregate_average_enrollment_time(self):
+        """Return the average number of days since enrollment across all courses."""
+        if not self._enrollments:
+            return 0
+        total_days = 0
+        now = datetime.now()
+        for enrollment in self._enrollments:
+            delta = now - enrollment.get_enrollment_date()
+            total_days += delta.days
+        return total_days / len(self._enrollments)
+
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -45,3 +61,12 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        """Return a dict with enrollment date (date only) as keys and count of enrollments on that date as values."""
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date_key = enrollment.get_enrollment_date().date()
+            enrollment_count[date_key] = enrollment_count.get(date_key, 0) + 1
+        return enrollment_count
